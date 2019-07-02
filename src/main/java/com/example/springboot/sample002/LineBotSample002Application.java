68e789c07c7bf952aae858d1aa44f7e371641623
tomatsu-yoshihiro <49357980+tomatsu-yoshihiro@users.noreply.github.com>
2019-07-02 23:02:59 +0900

    z

---
+++ src/main/java/com/example/springboot/sample002/LineBotSample002Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot.sample002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineBotSample002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineBotSample002Application.class, args);
+	}
+
+}
