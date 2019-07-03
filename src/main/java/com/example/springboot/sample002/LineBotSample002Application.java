091016b0869fa1acb6e658ce60aab52a8c38e1f2
tomatsu-yoshihiro <49357980+tomatsu-yoshihiro@users.noreply.github.com>
2019-07-03 16:59:58 +0900

    東京メトロAPIを組み込みました。

--- src/main/java/com/example/springboot/sample002/LineBotSample002Application.java
+++ src/main/java/com/example/springboot/sample002/LineBotSample002Application.java
@@ -1,11 +1,15 @@
 package com.example.springboot.sample002;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
+import com.example.springboot.sample002.dto.TrainInformationDto;
 import com.linecorp.bot.client.LineMessagingService;
 import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.Event;
@@ -20,8 +24,26 @@ import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 @LineMessageHandler
 public class LineBotSample002Application {
 	
+	static private final String API_ENDPOINT = "https://api.tokyometroapp.jp/api/v2/datapoints?rdf:type=odpt:TrainInformation&acl:consumerKey=b26ce611b4195e1ca43197c4ca9bc19082afe5b93a61bf4cb60036346f2cd797&odpt:railway={railway}";
+	
+	private static Map<String, String> railwayMap = new HashMap<String, String>() {
+	    {
+	        put("東西線", "odpt.Railway:TokyoMetro.Tozai");
+	        put("日比谷線", "odpt.Railway:TokyoMetro.Hibiya");
+	        put("千代田線", "odpt.Railway:TokyoMetro.Chiyoda");
+	        put("副都心線", "odpt.Railway:TokyoMetro.Fukutoshin");
+	        put("銀座線", "odpt.Railway:TokyoMetro.Ginza");
+	        put("半蔵門線", "odpt.Railway:TokyoMetro.Hanzomon");
+	        put("丸ノ内線", "odpt.Railway:TokyoMetro.Marunouchi");
+	        put("南北線", "odpt.Railway:TokyoMetro.Namboku");
+	        put("有楽町線", "odpt.Railway:TokyoMetro.Yurakucho");
+	    }
+	};
+	
 	@Autowired
     private LineMessagingService lineMessagingService;
+	
+	private RestTemplate restTemplate = new RestTemplate();
 
 	public static void main(String[] args) {
 		SpringApplication.run(LineBotSample002Application.class, args);
@@ -30,7 +52,12 @@ public class LineBotSample002Application {
 	
     @EventMapping
     public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {
-    	System.out.println("event: " + event.getMessage().getText());
+    	
+    	String railway = railwayMap.get(event.getMessage().getText());
+    	System.out.println("railway: " + railway);
+    	
+    	TrainInformationDto dto = restTemplate.getForObject(API_ENDPOINT, TrainInformationDto.class, railway);
+    	
         final BotApiResponse apiResponse = lineMessagingService
             .replyMessage(new ReplyMessage(event.getReplyToken(),
                                            Collections.singletonList(new TextMessage(event.getSource().getUserId()))))
