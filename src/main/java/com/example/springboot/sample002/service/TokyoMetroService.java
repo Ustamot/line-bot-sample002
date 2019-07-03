162023d9f5e253258bbe19237bb334edb597ddba
tomatsu-yoshihiro <49357980+tomatsu-yoshihiro@users.noreply.github.com>
2019-07-03 19:13:28 +0900

    いろいろ

--- src/main/java/com/example/springboot/sample002/service/TokyoMetroService.java
+++ src/main/java/com/example/springboot/sample002/service/TokyoMetroService.java
@@ -11,7 +11,7 @@ public class TokyoMetroService {
 	static private final String API_ENDPOINT = "https://api.tokyometroapp.jp/api/v2/datapoints?rdf:type=odpt:TrainInformation&acl:consumerKey=b26ce611b4195e1ca43197c4ca9bc19082afe5b93a61bf4cb60036346f2cd797&odpt:railway={railway}";
 	
 	@Autowired
-	@Qualifier("trainInfomationSearchRestTemplate")
+//	@Qualifier("trainInfomationSearchRestTemplate")
 	private RestTemplate restTemplate;
 	
 	public TokyoMetroService () {
