b94538cb93be1649e1cb345e48abf4212b871aee
tomatsu-yoshihiro <49357980+tomatsu-yoshihiro@users.noreply.github.com>
2019-07-03 19:16:55 +0900

    @ComponentScan追加

--- src/main/java/com/example/springboot/sample002/service/TokyoMetroService.java
+++ src/main/java/com/example/springboot/sample002/service/TokyoMetroService.java
@@ -1,11 +1,12 @@
 package com.example.springboot.sample002.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.springboot.sample002.dto.TrainInformationDto;
 
+@ComponentScan("com.example.springboot.sample002")
 public class TokyoMetroService {
 	
 	static private final String API_ENDPOINT = "https://api.tokyometroapp.jp/api/v2/datapoints?rdf:type=odpt:TrainInformation&acl:consumerKey=b26ce611b4195e1ca43197c4ca9bc19082afe5b93a61bf4cb60036346f2cd797&odpt:railway={railway}";
