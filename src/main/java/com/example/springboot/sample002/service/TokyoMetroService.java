9e40bcbcfbfa7a0654620e8d7a0b776c61dc792e
tomatsu-yoshihiro <49357980+tomatsu-yoshihiro@users.noreply.github.com>
2019-07-04 23:18:12 +0900

    misc

--- src/main/java/com/example/springboot/sample002/service/TokyoMetroService.java
+++ src/main/java/com/example/springboot/sample002/service/TokyoMetroService.java
@@ -1,13 +1,8 @@
 package com.example.springboot.sample002.service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.springboot.sample002.dto.TrainInformationDto;
@@ -18,7 +13,7 @@ public class TokyoMetroService {
 	static private final String API_ENDPOINT = "https://api.tokyometroapp.jp/api/v2/datapoints?rdf:type=odpt:TrainInformation&acl:consumerKey=b26ce611b4195e1ca43197c4ca9bc19082afe5b93a61bf4cb60036346f2cd797&odpt:railway={railway}";
 	
 	@Autowired
-//	@Qualifier("trainInfomationSearchRestTemplate")
+	@Qualifier("trainInfomationSearchRestTemplate")
 	private RestTemplate restTemplate;
 	
 	public TokyoMetroService () {
@@ -27,7 +22,7 @@ public class TokyoMetroService {
 	
 	public TrainInformationDto[] getTrainInfomation(String railway) {
 		
-		restTemplate = new RestTemplate();
+/////		restTemplate = new RestTemplate();
 //		MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
 //		List<MediaType> supportedMediaTypes = new ArrayList<>(messageConverter.getSupportedMediaTypes());
 //		supportedMediaTypes.add(MediaType.TEXT_PLAIN); // text/plainのJacksonの処理対象にくわえる
