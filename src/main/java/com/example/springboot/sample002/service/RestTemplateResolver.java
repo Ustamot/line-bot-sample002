e152ba9716c2bb79e1f2b3e5d750449780f62763
tomatsu-yoshihiro <49357980+tomatsu-yoshihiro@users.noreply.github.com>
2019-07-04 23:34:07 +0900

    misc

--- src/main/java/com/example/springboot/sample002/service/RestTemplateResolver.java
+++ src/main/java/com/example/springboot/sample002/service/RestTemplateResolver.java
@@ -16,12 +16,13 @@ public class RestTemplateResolver {
 	@Bean
 	public RestTemplate trainInfomationSearchRestTemplate() {
 		System.out.println("##### trainInfomationSearchRestTemplate");
-		RestTemplate restTemplate = new RestTemplate();
-		MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
-		List<MediaType> supportedMediaTypes = new ArrayList<>(messageConverter.getSupportedMediaTypes());
-		supportedMediaTypes.add(MediaType.TEXT_PLAIN); // text/plainのJacksonの処理対象にくわえる
-		messageConverter.setSupportedMediaTypes(supportedMediaTypes);
-		restTemplate.setMessageConverters(Collections.singletonList(messageConverter)); // カスタムしたHttpMessageConverterを適用
+//		RestTemplate restTemplate = new RestTemplate();
+//		MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+//		List<MediaType> supportedMediaTypes = new ArrayList<>(messageConverter.getSupportedMediaTypes());
+//		supportedMediaTypes.add(MediaType.TEXT_PLAIN); // text/plainのJacksonの処理対象にくわえる
+//		messageConverter.setSupportedMediaTypes(supportedMediaTypes);
+//		restTemplate.setMessageConverters(Collections.singletonList(messageConverter)); // カスタムしたHttpMessageConverterを適用
+		RestTemplate restTemplate = new RestTemplate(); 
 		return restTemplate;
 	}
 
